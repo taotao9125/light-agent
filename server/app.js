131b9720bf3be56dc987072355e771cf1cf6075a
blue <tuopo91@gmail.com>
2026-04-18 20:38:51 +0800

    cjs -> es

--- server/app.js
+++ server/app.js
@@ -1,16 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-require("dotenv").config();
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const mysql = require('mysql2/promise')
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+dotenv.config();
 
-var app = express();
+const app = express();
+
+// 获取 __dirname（ES module 中需要手动构造）
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,42 +32,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
- 
-// 如何验证  createConnection， createPool
+// 如何验证 createConnection， createPool
 // const pool = mysql.createPool({
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER,
 //   database: process.env.DB_BASE,
 // })
 
-// 每次都连接？
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_BASE,
-})
+// 每次都连接？注意：createConnection 返回 Promise，需要 await 或 .then
+// 建议改成 createPool 或者用 await 连接
+let connection;
+const initConnection = async () => {
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_BASE,
+    });
+  }
+  return connection;
+};
 
-
-app.get('/user', (req, res) => {
-  // 每次都查询？
-  connection
-  .then(q => {
-    q.query(
-      'SELECT * from `users`'
-    ).then(r => {
-      const [data] = r;
-      res.send(data) 
-    })
-  })
-})
+app.get('/user', async (req, res) => {
+  try {
+    const conn = await initConnection();
+    const [data] = await conn.query('SELECT * FROM `users`');
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Database query failed' });
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-
-
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -73,10 +80,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-
-
-
-
-
-module.exports = app;
+export default app;
