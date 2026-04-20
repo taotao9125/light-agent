65f7671296b2b0d1dfbc7e2b70547ebc3f973ef7
blue <tuopo91@gmail.com>
2026-04-20 17:32:01 +0800

    给 wrapres添加一些错误流转注释

--- server/lib/wrapRes.js
+++ server/lib/wrapRes.js
@@ -1,3 +1,15 @@
+// 它本来是做“统一成功响应格式”的，但顺手也承担了“把 async 异常转交给 Express 错误中间件”的职责。
+// return -> wrapRes 统一成功响应 throw  -> wrapRes catch -> next(err) -> 全局错误中间件
+// 在同步代码里，throw new error, 会被 app.use(err) 捕捉到。在异步代码里，不会被捕捉到
+/**
+ * 
+handler throw
+-> wrapRes catch
+-> next(err)
+-> global error middleware
+-> 统一 error response
+ */
+
 const wrap = (fn) => async (req, res, next) => {
   try {
     const result = await fn(req, res, next);
